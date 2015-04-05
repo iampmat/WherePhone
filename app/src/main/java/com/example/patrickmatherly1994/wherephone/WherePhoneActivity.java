79a7223b4c4d03bd231ee7d9cd99cd96d73cdb34
iampmat <patrickmatherly1994@gmail.com>
2015-04-04 22:08:52 -0400

    Runs in an activity

--- app/src/main/java/com/example/patrickmatherly1994/wherephone/WherePhoneActivity.java
+++ app/src/main/java/com/example/patrickmatherly1994/wherephone/WherePhoneActivity.java
@@ -3,11 +3,13 @@ package com.example.patrickmatherly1994.wherephone;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
@@ -23,13 +25,26 @@ public class WherePhoneActivity extends Activity implements RecognitionListener 
     private SpeechRecognizer recognizer;
     private static final String KEYPHRASE = "where is my phone";
 
+    private TextToSpeech reply;
+
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
+        setContentView(R.layout.activity_where_phone);
+
+        // Set up the texttospeech on reply
+        reply=new TextToSpeech(getApplicationContext(),
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status != TextToSpeech.ERROR){
+                            reply.setLanguage(Locale.UK);
+                        }
+                    }
+                });
 
         // Recognizer initialization, Include resource files in form of assets
-        // Call swithsearch on the keyphrase
-
+        // Call switchsearch on the keyphrase
         new AsyncTask<Void, Void, Exception>() {
             @Override
             protected Exception doInBackground(Void... params) {
@@ -98,6 +113,7 @@ public class WherePhoneActivity extends Activity implements RecognitionListener 
     public void onEndOfSpeech() {
         if (!recognizer.getSearchName().equals(KEYPHRASE))
             switchSearch(KEYPHRASE);
+        ((TextView) findViewById(R.id.result_text)).setText("");
     }
 
     @Override
@@ -106,11 +122,14 @@ public class WherePhoneActivity extends Activity implements RecognitionListener 
             return;
 
         String text = hypothesis.getHypstr();
-        if (text.equals(KEYPHRASE))
+        if (text.equals(KEYPHRASE)) {
             // set result_text to the partial result
             ((TextView) findViewById(R.id.result_text)).setText(text);
-        else
-            ((TextView) findViewById(R.id.result_text)).setText(text);
+            reply.speak("I am here", TextToSpeech.QUEUE_ADD, null);
+            if(!reply.isSpeaking()){ recognizer.stop(); }
+            switchSearch(KEYPHRASE);
+        }
+        else{ ((TextView) findViewById(R.id.result_text)).setText(text); }
     }
 
     @Override
@@ -131,4 +150,25 @@ public class WherePhoneActivity extends Activity implements RecognitionListener 
     public void onTimeout() {
         switchSearch(KEYPHRASE);
     }
+
+
+    // Used for the texttospeech reply
+    @Override
+    public void onPause(){
+        if(reply !=null){
+            reply.stop();
+            reply.shutdown();
+        }
+        super.onPause();
+    }
+
+    /*
+    // Used to execute the text to speech
+    public void speakText(){
+        String toSpeak = "I am here";
+        Toast.makeText(getApplicationContext(), toSpeak,
+                Toast.LENGTH_SHORT).show();
+        reply.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    */
 }
